8c125285b785bc1255187dc28497832afb2fee4b
xlmliquidity <xlmliquidity@protonmail.com>
2022-02-25 20:06:55 +0200

    add new files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="App">
+        <div className="card">
+          <div className="floating first"></div>
+          <div className="floating second"></div>
+          <button>META</button>
+          <h1></h1>
+
+          <h2></h2>
+        </div>
+      </div>
+    </main>
   );
 }
-
-export default App;
